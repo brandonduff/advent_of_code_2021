3abbc10a113dcca257df7116415f96594cbac0a9
Brandon Duff <brandonduff17@gmail.com>
2021-12-03 21:34:45 -0700

    Finish refactoring Day 3

--- day_three_test.rb
+++ day_three_test.rb
@@ -14,15 +14,15 @@ class DayThree
   end
 
   def part_two
-    OxygenGeneratorRating.new(binaries).to_i * Co2ScrubberRating.new(binaries).to_i
-  end
-
-  def binaries
-    Binaries.new(input.split("\n"))
+    Rating.for_oxygen(rows).to_i * Rating.for_c02(rows).to_i
   end
 
   def cols
-    binaries.cols
+    rows.map(&:chars).transpose
+  end
+
+  def rows
+    input.split("\n")
   end
 
   class Binaries
@@ -30,13 +30,18 @@ class DayThree
 
     attr_reader :bit_criteria
 
-    def initialize(input)
-      @input = input
-      @bit_criteria = 0
+    def self.for_oxygen(input)
+      new(input, RatingInstrument.oxygen)
     end
 
-    def to_binaries
-      self
+    def self.for_c02(input)
+      new(input, RatingInstrument.c02)
+    end
+
+    def initialize(input, instrument_config)
+      @input = input
+      @bit_criteria = 0
+      @instrument_config = instrument_config
     end
 
     def each(*args, &block)
@@ -47,33 +52,34 @@ class DayThree
       map(&:chars).transpose
     end
 
-    def iterate(instrument_config)
-      generator_rating = generator_rating(instrument_config)
+    def iterate
+      generator_rating = self.generator_rating
       @input = select { |binary| binary[bit_criteria] == generator_rating }
       @bit_criteria += 1
-      self
     end
 
     def reduced?
       @input.length == 1
     end
 
-    def generator_rating(instrument_config)
-      instrument_config.rating_for(cols[bit_criteria])
+    def generator_rating
+      @instrument_config.rating_for(cols[bit_criteria])
     end
   end
 
-  InstrumentConfig = Struct.new(:tie_winning_number, :tie_losing_number, :comparison) do
-    def rating_for(binary)
-      binary.count(tie_losing_number).send(comparison, binary.count(tie_winning_number)) ? tie_losing_number : tie_winning_number
-    end
-  end
-
-  class OxygenGeneratorRating
+  class Rating
     attr_reader :binaries
 
+    def self.for_oxygen(rows)
+      new(Binaries.for_oxygen(rows))
+    end
+
+    def self.for_c02(rows)
+      new(Binaries.for_c02(rows))
+    end
+
     def initialize(binaries)
-      @binaries = binaries.to_binaries
+      @binaries = binaries
     end
 
     def to_i
@@ -81,37 +87,23 @@ class DayThree
     end
 
     def reduction
-      binaries.iterate(InstrumentConfig.new(tie_winning_number, tie_losing_number, comparison)) until binaries.reduced?
+      binaries.iterate until binaries.reduced?
       binaries
     end
-
-    def tie_losing_number
-      "0"
-    end
-
-    def tie_winning_number
-      tie_losing_number == "0" ? "1" : "0"
-    end
-
-    def comparison
-      :>
-    end
   end
 
-  class Co2ScrubberRating < OxygenGeneratorRating
-    def tie_losing_number
-      "1"
+  RatingInstrument = Struct.new(:tie_winning_number, :tie_losing_number, :comparison) do
+    def self.oxygen
+      new("1", "0", :>)
     end
 
-    def comparison
-      :<
+    def self.c02
+      new("0", "1", :<)
     end
-  end
-end
 
-class Array
-  def to_binaries
-    DayThree::Binaries.new(self)
+    def rating_for(binary)
+      binary.count(tie_losing_number).send(comparison, binary.count(tie_winning_number)) ? tie_losing_number : tie_winning_number
+    end
   end
 end
 
@@ -139,22 +131,22 @@ class DayThreeTest < Minitest::Test
   end
 
   def test_oxygen_generator_rating_with_single_value
-    assert_equal 0b00100, DayThree::OxygenGeneratorRating.new(["00100"]).to_i
+    assert_equal 0b00100, DayThree::Rating.for_oxygen(["00100"]).to_i
   end
 
   def test_oxygen_generator_rating_two_values
     binaries = ["00001", "11111"]
-    assert_equal 0b11111, DayThree::OxygenGeneratorRating.new(binaries).to_i
+    assert_equal 0b11111, DayThree::Rating.for_oxygen(binaries).to_i
   end
 
   def test_oxygen_generator_rating_many_values
     binaries = ["00001", "10110", "11111"]
-    assert_equal 0b11111, DayThree::OxygenGeneratorRating.new(binaries).to_i
+    assert_equal 0b11111, DayThree::Rating.for_oxygen(binaries).to_i
   end
 
   def test_co2_scrubber_rating
     binaries = ["00011", "11110", "11111"]
-    assert_equal 0b00011, DayThree::Co2ScrubberRating.new(binaries).to_i
+    assert_equal 0b00011, DayThree::Rating.for_c02(binaries).to_i
   end
 
   def test_life_support_rating
